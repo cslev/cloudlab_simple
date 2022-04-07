f7443d157e47e60eb6d2dfdee5b2c33f39974ea6
cslev <cs.lev@gmx.com>
2022-04-07 15:09:36 +0800

    minor changes

--- profile.py
+++ profile.py
@@ -7,6 +7,7 @@ kube_instruction= \
 Check the following github repo for more information
 https://github.com/cslev/cloudlab_simple
 """
+
 import geni.portal as portal
 import geni.rspec.pg as pg
 # Import the Emulab specific extensions.
@@ -27,16 +28,16 @@ request.addTour(tour)
 # node quic on UTAH cluster
 for i in range(1,7):
   key = str("quic_docker_{}".format(i))
-  kube_quic = request.RawPC(str(key))
-  # kube_quic.hardware_type = "m400" ##ARM - Eight 64-bit ARMv8 (Atlas/A57) cores at 2.4 GHz (APM X-GENE)
-  #kube_quic.hardware_type = "xl170" ##Intel x86_64 - 	Ten-core Intel E5-2640v4 at 2.4 GHz
-  kube_quic.hardware_type = "d6515"  ##AMD x86_64 -  AMD EPYC ROMTE, 32-cre AMD 7452 @ 2.35GHz
+  node = request.RawPC(str(key))
+  # node.hardware_type = "m400" ##ARM - Eight 64-bit ARMv8 (Atlas/A57) cores at 2.4 GHz (APM X-GENE)
+  node.hardware_type = "xl170" ##Intel x86_64 - 	Ten-core Intel E5-2640v4 at 2.4 GHz
+  # node.hardware_type = "d6515"  ##AMD x86_64 -  AMD EPYC ROMTE, 32-cre AMD 7452 @ 2.35GHz
   #kube_doh.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:DEB8-64-STD' #<-- does not work
-  kube_quic.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
-  kube_quic.Site(str(key))
+  node.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
+  node.Site(str(key))
 
   #START the docker-based version - this init scripts install basic packages and docker subsystem to make it capable running containers as well
-  kube_quic.addService(pg.Execute(shell="bash", command="/local/repository/cloudlabs_start_docker.sh"))
+  node.addService(pg.Execute(shell="bash", command="/local/repository/cloudlabs_start_docker.sh"))
 
 portal.context.printRequestRSpec()
 
